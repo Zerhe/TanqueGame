aab8dbe544cc9a5752ba3bbc4367d771271b7b48
Zerhe <dante_ro@hotmail.com.ar>
2017-04-19 16:20:48 -0300

    Plataforma que rota

--- Tanque/Assets/Scripts/Rotar.cs
+++ Tanque/Assets/Scripts/Rotar.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class Rotar : MonoBehaviour {
     public int velRot;
+    float timer;
+    float timer2;
 	void Start () {
-		
+        timer = 0;
+        timer2 = 0;
 	}
 
 	void Update () {
-        transform.Rotate(Vector3.up * Time.deltaTime * velRot);
-	}
+        if (timer <= 2.24)
+        {
+            timer += Time.deltaTime;
+            transform.Rotate(Vector3.up * Time.deltaTime * velRot);
+
+        }
+        else if (timer >2.24)
+        {
+            timer2 += Time.deltaTime;
+
+        }
+        if (timer2 >= 10)
+        {
+            timer = 0;
+            timer2 = 0;
+        }
+        print(timer);
+        print(timer2);
+    }
 }
