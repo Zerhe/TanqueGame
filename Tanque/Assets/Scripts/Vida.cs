add80e2ba64d685a4c58f54b4442ded22bc479c1
Zerhe <dante_ro@hotmail.com.ar>
2017-05-23 19:55:03 -0300

    AgregadaBarraDeVida
    agregada la barra de vida

--- Tanque/Assets/Scripts/Vida.cs
+++ Tanque/Assets/Scripts/Vida.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vida : MonoBehaviour {
     public int cantVida;
+    [SerializeField]
+    private Slider barraVida;
     	
 	void Update ()
     {
-        
+        //barraVida.value = Mathf.MoveTowards(barraVida.value, cantVida, 0.15f);
+        barraVida.value = cantVida;
 		if(cantVida <= 0)
         {
             gameObject.SetActive(false);
